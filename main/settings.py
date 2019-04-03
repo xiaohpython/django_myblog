2968cd4852a51ed24a084ea9a1e40282dd07a926
python <43243692+xiaohpython@users.noreply.github.com>
2019-04-03 18:14:22 +0800

    Update settings.py

--- main/settings.py
+++ main/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'de6x6i!%w0n#0urcwanj&%+t41i@m^y!0=n23cl*4zo#0u&9&q'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 if DEBUG:
     from socket import gethostname, gethostbyname
     ALLOWED_HOSTS = [
